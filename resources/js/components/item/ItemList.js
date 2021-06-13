1a521e6489d890fd5a4fd54744433a9baf51adba
irshath1022im <48007558+irshath1022im@users.noreply.github.com>
2021-06-13 12:32:51 +0300

    fix item image size

--- resources/js/components/item/ItemList.js
+++ resources/js/components/item/ItemList.js
@@ -4,11 +4,12 @@ export default function ItemList(props) {
     const {erp_code,name,category,thumbnail,initial_qty} = props.items;
     return (
         <>
-                <tr>
+                <tr className="item">
                     <td scope="row">{erp_code}</td>
                     <td>{name}</td>
                     <td>{category}</td>
-                    <td><img className="w-25" src={`/storage/${thumbnail}`} /></td>
+                    <td
+                    className="item-thumbnail"><img className="" src={`/storage/${thumbnail}`} /></td>
                     <td>{initial_qty}</td>
 
 
